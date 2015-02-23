0d7b653ff3176df0e75ede1211f4e98d14fc4aff
mhwelander <maw@sitecore.net>
2015-02-23 15:08:21 +0000

    Initial commit.

---
+++ markers.js
@@ -0,0 +1,73 @@
+// Add your own markers to this file.
+
+var MAPCRAFTER_MARKERS = [
+	
+	// just one example marker group
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "subterritories",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Sub-Territories",
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"novapeak" : [
+				// example marker, pretty format:
+				{
+					// position ([x, z, y])
+					"pos" : [-206, 107, 64],
+					// title when you hover over the marker
+					"title" : "Nova Peak",
+					// text in the marker popup window
+					"text" : "Nova Peak"
+				},
+								{
+					// position ([x, z, y])
+					"pos" : [-73, 12, 64],
+					// title when you hover over the marker
+					"title" : "The Northwoods",
+					// text in the marker popup window
+					"text" : "The Northwoods"
+				},
+								{
+					// position ([x, z, y])
+					"pos" : [-90, -251, 64],
+					// title when you hover over the marker
+					"title" : "Hogbelly Hills",
+					// text in the marker popup window
+					"text" : "Hogbelly Hills"
+				},
+												{
+					// position ([x, z, y])
+					"pos" : [-80, 168, 64],
+					// title when you hover over the marker
+					"title" : "Sawtooth Edge",
+					// text in the marker popup window
+					"text" : "Sawtooth Edge"
+				}
+			],
+		},
+	},
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "nsubterritories",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Neutral Sub-Territories",
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"novapeak" : [
+				// example marker, pretty format:
+				{
+					// position ([x, z, y])
+					"pos" : [268, -73, 64],
+					// title when you hover over the marker
+					"title" : "The Tangle",
+					// text in the marker popup window
+					"text" : "The Tangle",
+					"color" : "yellow"
+				}
+			],
+		},
+	}
+];
